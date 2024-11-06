22b554ff96a610af6882dd9b7beb1bf5e2c9a0ee
dindz <dindoquitor@gmail.com>
2024-11-06 21:37:49 +0800

    fix error in code.

--- setup_adguard.sh
+++ setup_adguard.sh
@@ -13,9 +13,19 @@ rm /tmp/adguardhome.tar.gz
 # Navigate to the AdGuardHome directory
 cd /opt/AdGuardHome
 
-# Run the setup wizard with custom ports
-echo "Running the setup wizard on custom ports..."
-./AdGuardHome -s install --web-port 8081 --dns-port 5353
+# Run the setup wizard without specifying ports (default ports will be used initially)
+echo "Running the setup wizard..."
+./AdGuardHome -s install
+
+# Wait for the installation to complete
+sleep 5
+
+# Modify the configuration file to use custom ports
+echo "Modifying configuration file for custom ports..."
+
+# Change the web interface port and DNS port (8081 and 5353 as example)
+sed -i 's/"http_port": 3000/"http_port": 8081/' /opt/AdGuardHome/AdGuardHome.yaml
+sed -i 's/"dns_port": 53/"dns_port": 5353/' /opt/AdGuardHome/AdGuardHome.yaml
 
 # Create a service script for AdGuard Home
 echo "Creating service script for AdGuard Home..."
