05e666e336fc8df15f0b3dc85c4452a1b5d69ead
dindz <107230909+dindz@users.noreply.github.com>
2024-12-19 19:57:36 +0800

    Update setup_server.sh

--- setup_server.sh
+++ setup_server.sh
@@ -5,8 +5,8 @@ apk update && apk upgrade
 
 # Configure static IP address for eth0
 cat <<EOF > /etc/network/interfaces
-auto eth0
-iface eth0 inet static
+auto eth3
+iface eth3 inet static
     address 192.168.254.108
     netmask 255.255.255.0
     gateway 192.168.254.254
@@ -20,7 +20,7 @@ for service in $(rc-service -l | grep -E '(net|dhcp|dns|network)'); do
     rc-service $service restart
 done
 # Verify the new IP configuration
-ip a show eth0
+ip a show eth3
 
 echo "Static IP configuration complete and network services restarted."
 
